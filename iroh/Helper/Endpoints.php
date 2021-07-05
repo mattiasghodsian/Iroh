5c73298e8a4eeb6848e394fc4c63851d66ef15ce
MattiasG <mattiasghodsian@gmail.com>
2021-07-05 19:35:07 +0200

    base

---
+++ iroh/Helper/Endpoints.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This Helper class simplifys adding API Endpoints
+ * 
+ * @package Iroh_Endpoints
+ * @author Mattias Ghodsian
+ * @source https://github.com/mattiasghodsian/Iroh
+ * @license GPL 2.0
+ */
+
+namespace Helper;
+
+class Endpoints {
+
+    private $actions;
+
+    /**
+     * construct
+     * 
+     * @since 1.0
+     * @return void
+     */
+    public function __construct(){
+
+    }
+
+}
