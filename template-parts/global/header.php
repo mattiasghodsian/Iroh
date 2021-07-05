fa103e15f6bacd9722e5d19ce7de58d02a833791
MattiasG <mattias.g@provideit.se>
2021-07-05 17:23:17 +0200

    replaced menu with botstrap navwalker

--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -1,7 +1,12 @@
 <?php 
     wp_nav_menu([
-        'menu' => 'main-navigation', 
-        'container_class' => 'menu-main-navigation-container', 
-        'menu_class' => 'menu'
+        'theme_location'  => 'primary',
+        'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+        'container'       => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id'    => 'bs-example-navbar-collapse-1',
+        'menu_class'      => 'navbar-nav mr-auto',
+        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'          => new WP_Bootstrap_Navwalker(),
     ]);
 ?>
