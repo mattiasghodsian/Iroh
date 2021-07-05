46f5bb06b308b959b5166b3f1c19c5d1e2a1c531
MattiasG <mattias.g@provideit.se>
2021-07-05 17:24:23 +0200

    include wp-bootstrap-navwalker

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ define( "IROH_URI", get_stylesheet_directory_uri() . '/' );
 /* Composer */
 require_once( IROH_PATH . 'vendor/autoload.php' );
 
+/* WP Bootstrap NavWalker */
+require_once( IROH_PATH . 'vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php' );
+
 /* Includes */
 require_once( IROH_PATH . 'includes/theme.php' );
 require_once( IROH_PATH . 'includes/examples.php' );
