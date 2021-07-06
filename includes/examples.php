a2391a308cef196457ba58e07905de1657292b0d
MattiasG <mattiasghodsian@gmail.com>
2021-07-06 13:35:17 +0200

    Cronjob Helper

--- includes/examples.php
+++ includes/examples.php
@@ -64,5 +64,31 @@ $target = [
         ],
     ]
 ];
-$arr_handler = new \Helper\Arr\Handler();
-dump( $arr_handler->data_get($target, 'kids.0.name') );
+// $arr_handler = new \Helper\Arr\Handler();
+// dump( $arr_handler->data_get($target, 'kids.0.name') );
+
+/**
+ * Register cronjob task
+ */
+$cron = new \Helper\Cronjob();
+
+$cron->add_recurrence([
+    'key'       => 'iroh_every_thirty_min', 
+    'interval'  => 1800,
+    'display'   => __( 'Every 30 Minutes', 'iroh' )
+]);
+
+
+function test_cron(){
+    echo 'Iroh Cron';
+}
+
+$cron->add_event([
+    'hook'          => 'test_cron',
+    'args'          => [],
+    'timestamp'     => time(),
+    'recurrence'    => 'iroh_every_thirty_min',
+    'wp_error'      => false,
+]);
+
+dump( $cron->registry() );
