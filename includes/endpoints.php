71f529601bddbc97785b2f823beb78d94df148c8
Martin <martin.m@provideit.se>
2021-07-06 16:43:36 +0200

    feature/helpers

--- includes/endpoints.php
+++ includes/endpoints.php
@@ -7,21 +7,21 @@
  * @return void
  */
 
-use \Helper\Endpoints;
+// use \Helper\Endpoints;
 
-$endpoints = new Endpoints;
+// $endpoints = new Endpoints;
 
-$endpoints->buildArray('iroh', '/iroh-route', array(
-    'methods'             => 'POST',
-    'callback'            => 'iroh_example_function',
-    'permission_callback' => '__return_true',
-))->buildArray('iroh', '/iroh-route-1', array(
-    'methods'             => 'GET',
-    'callback'            => 'iroh_example_function',
-    'permission_callback' => '__return_true',
-))->addTheActions();
+// $endpoints->buildArray('iroh', '/iroh-route', array(
+//     'methods'             => 'POST',
+//     'callback'            => 'iroh_example_function',
+//     'permission_callback' => '__return_true',
+// ))->buildArray('iroh', '/iroh-route-1', array(
+//     'methods'             => 'GET',
+//     'callback'            => 'iroh_example_function',
+//     'permission_callback' => '__return_true',
+// ))->addTheActions();
 
-function iroh_example_function()
-{
-    return wp_send_json('iroh', 200);
-}
+// function iroh_example_function()
+// {
+//     return wp_send_json('iroh', 200);
+// }
