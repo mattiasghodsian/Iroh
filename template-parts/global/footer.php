0a264655736c291382c13b55f8073c83c015ac21
rakma <mattiasghodsian@gmail.com>
2021-08-08 00:38:04 +0200

    minor changes

--- template-parts/global/footer.php
+++ template-parts/global/footer.php
@@ -5,20 +5,6 @@
                 <img src="<?= APP_URI; ?>/assets/img/iroh.png" alt="Iroh" class="ft-logo">
             </p>
             <small class="mb-3 text-center text-secondary">Iroh The WordPress Starter Theme For Developers</small>
-            <p>
-                <a href="https://github.com/mattiasghodsian/Iroh/blob/main/README.md">
-                    <img alt="GPL-2.0 License" src="https://img.shields.io/badge/license-GPL--2.0-purple/?style=flat-square" />
-                </a>
-                <a href="https://github.com/mattiasghodsian/Iroh/">
-                    <img alt="Build Status" src="https://img.shields.io/github/stars/mattiasghodsian/iroh?style=flat-square" />
-                </a>
-                <a href="https://github.com/mattiasghodsian/Iroh/">
-                    <img alt="Contributors" src="https://img.shields.io/github/contributors/mattiasghodsian/iroh?style=flat-square">  
-                </a>
-                <a href="LICENSE">
-                    <img alt="Donate Ethereum" src="https://img.shields.io/static/v1?label=donate&message=ethereum&color=blue&style=flat-square" />
-                </a>
-            </p>
         </div>
     </div>
 </div>
