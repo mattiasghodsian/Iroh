62e03dc1c5aa2202e9f6416759dd1cc3a3f27b5d
MattiasG <mattiasghodsian@gmail.com>
2021-08-29 12:47:26 +0200

    base class

---
+++ src/Helper/Theme/Widget.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This Helper class simplifys adding Widgets
+ * 
+ * @package Iroh_Widget
+ * @author Mattias Ghodsian
+ * @source https://github.com/mattiasghodsian/Iroh
+ * @license GPL 2.0
+ */
+
+namespace Helper;
+
+class Cronjob {
+
+
+    /**
+     * construct
+     * 
+     * @return void
+     */
+    public function __construct(){
+
+
+    }
+
+}
