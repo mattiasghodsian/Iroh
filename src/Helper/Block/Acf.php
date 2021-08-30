74f180e6d0415d11888f9c4fc811dac3bacd50ec
Mattias <mattiasghodsian@gmail.com>
2021-08-30 14:01:48 +0200

    Render callback
    - Replaced block-{slug}.php to {slug}.php 
    - minor tweaks

--- src/Helper/Block/Acf.php
+++ src/Helper/Block/Acf.php
@@ -12,9 +12,6 @@
 
 namespace Helper\Block;
 
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Validation;
-
 class Acf {
     private $blocks;
 
@@ -24,7 +21,8 @@ class Acf {
      * @since 1.0
      * @return void
      */
-    public function __construct(){
+    public function __construct()
+    {
         if (function_exists('acf_register_block')) {
             add_action('init', array($this, 'register'));
         }
@@ -37,11 +35,11 @@ class Acf {
      * @param array $block
      * @return void
      */
-    private function render($block){
+    public function render($block)
+    {
         $slug = str_replace('acf/', '', $block['name']);
-
-        if (file_exists(APP_PATH."/templates/parts/acf-blocks/block-{$slug}.php")) {
-            include (APP_PATH."/templates/parts/acf-blocks/block-{$slug}.php");
+        if (file_exists(APP_PATH."templates/parts/acf-blocks/{$slug}.php")) {
+            include (APP_PATH."templates/parts/acf-blocks/{$slug}.php");
         }
     }
 
@@ -51,17 +49,14 @@ class Acf {
      * @since 1.0
      * @return void
      */
-    public function add($array){
-        // $blocks = [
-        //     [
-        //         'name'              => 'example',
-        //         'title'             => __('Example', 'iroh'),
-        //         'description'       => __('Example description', 'iroh'),
-        //         'render_callback'   => [$this, 'render'],
-        //         'mode'              => 'edit'
-        //     ]
-        // ];
-        $this->blocks = $array;
+    public function add(array $array)
+    {
+        foreach ($array as $key => $block) {
+            if ( !array_key_exists( 'render_callback', $block) ) {
+                $block['render_callback'] = array($this, 'render');
+            }
+            $this->blocks[] = $block;
+        }
     }
 
     /**
@@ -70,13 +65,11 @@ class Acf {
      * @since 1.0
      * @return void
      */
-    public function register(){
+    public function register()
+    {
         if ( $this->blocks ){
             foreach ($this->blocks as $key => $block) {
-                if ( !array_key_exists( 'render_callback', $block) ) {
-                    $block['render_callback'] = [$this, 'render'];
-                }
-                acf_register_block($block);
+                acf_register_block_type($block);
             }
         }
     }
